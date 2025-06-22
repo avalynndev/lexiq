6daaaea809139c451d7853499d80e335aadcce72
Avalynn <100826194+avalynndev@users.noreply.github.com>
2025-06-22 18:13:56 +0530

    fix params

--- app/(marketing)/user/[username]/page.tsx
+++ app/(marketing)/user/[username]/page.tsx
@@ -1,10 +1,7 @@
 import { UserProfileClient } from "./user-profile-client";
+import { useParams } from "next/navigation";
 
-export default async function UserProfilePage({
-  params,
-}: {
-  params: { username: string };
-}) {
-  const { username } = await params;
-  return <UserProfileClient username={username} />;
+export default async function UserProfilePage() {
+  const params = useParams<{ slug: string }>();
+  return <UserProfileClient username={params.slug} />;
 }
