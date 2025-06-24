2196254063b0ca05bf1f0eba0bf617aeed33c388
Avalynn <100826194+avalynndev@users.noreply.github.com>
2025-06-24 19:26:04 +0530

    remove not workign glow

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,16 +5,11 @@ import { FeaturesSection } from "@/components/features-section";
 import { TestimonialsSection } from "@/components/testimonials-section";
 import { CTA } from "@/components/cta";
 import { AnimatedHeroImages } from "@/components/animated-hero-images";
-import Glow from "@/components/ui/hero-glow";
 
 export default function Home() {
   return (
     <div className="min-h-screen overflow-hidden relative">
-      <Hero />{" "}
-      <Glow
-        variant="top"
-        className="animate-appear-zoom opacity-0 delay-1000"
-      />
+      <Hero />
       <AnimatedHeroImages />
       <FeaturesSection />
       <TestimonialsSection />
