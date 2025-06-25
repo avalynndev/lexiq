3dfa5a1afa191801479cfc2c7ef25fb2140aa78b
Avalynn <100826194+avalynndev@users.noreply.github.com>
2025-06-25 15:58:54 +0530

    added next toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "@/components/providers";
 import { Toaster } from "sonner";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { siteConfig } from "@/config/site";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: {
@@ -80,6 +81,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader
+            color="#2299DD"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={true}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          />
           <Providers>{children}</Providers>
           <Toaster richColors />
           <TailwindIndicator />
